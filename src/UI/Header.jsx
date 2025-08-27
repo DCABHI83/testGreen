50710731482b70e3bc57c067aad81e895bfe781f
DCABHI83 <araj10569@gmail.com>
2025-08-27 23:09:56 +0530

    fix image typo

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/icon.png';
 
 const Header = () => {
   const [activeSection, setActiveSection] = useState("");
